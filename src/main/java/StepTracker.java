112fb2db81c624b17156838cd1e3d67cd7aa92a8
EliPerman <145874379+EliPerman@users.noreply.github.com>
2024-04-22 22:07:03 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -4,7 +4,7 @@ public class StepTracker
  private int min;
  private ArrayList<Integer> log;
  
- public StepTracket(int MIN){
+ public StepTracker(int MIN){
   min = MIN;
   log = new ArrayList<Integer>();
  }
